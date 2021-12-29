8e439e26c2ad827c482782da8594115b5e039524
Sujatabatra <sujatabatra@hotmail.com>
2021-12-29 12:33:28 +0530

    Add files via upload

--- Day8 (Unit Testing)/MySecondJunitProject/src/com/sujata/test/TestMyCalculator.java
+++ Day8 (Unit Testing)/MySecondJunitProject/src/com/sujata/test/TestMyCalculator.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.sujata.demo.MyCalculator;
@@ -25,36 +26,43 @@ class TestMyCalculator {
 		myCalculator=null;
 	}
 
+	@Tag("Dev")
 	@Test
 	void t001() throws NegativeNumberException {
 		assertEquals(5, myCalculator.divide(20, 4));
 	}
 
+	@Tag("Prod")
 	@Test
 	void t002()throws NegativeNumberException {
 		assertThrows(NegativeNumberException.class, ()->myCalculator.divide(20, -4));
 	}
 	
+	@Tag("Prod")
 	@Test
 	void t003()throws NegativeNumberException {
 		assertThrows(NegativeNumberException.class, ()->myCalculator.divide(-20, 4));
 	}
 	
+	@Tag("Dev")
 	@Test
 	void t004()throws NegativeNumberException {
 		assertThrows(NegativeNumberException.class, ()->myCalculator.divide(-20, -4));
 	}
 	
+	@Tag("Prod")
 	@Test
 	void t005()throws NegativeNumberException {
 		assertThrows(ArithmeticException.class, ()->myCalculator.divide(20, 0));
 	}
 	
+	@Tag("Testing")
 	@Test
 	void t006()throws NegativeNumberException {
 		assertEquals(0, myCalculator.divide(0, 4));
 	}
 	
+	@Tag("Dev")
 	@Test
 	void t007()throws NegativeNumberException {
 		assertThrows(NegativeNumberException.class, ()->myCalculator.divide(-20, 0));
