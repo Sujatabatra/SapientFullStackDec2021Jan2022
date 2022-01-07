1d31a95048533dbe98c3e0e485f8cc66b8a7f600
Sujatabatra <sujatabatra@hotmail.com>
2022-01-07 10:32:59 +0530

    Add files via upload

---
+++ Day 14(Java Script)/DataTypeAnd Variable.js
@@ -0,0 +1,21 @@
+var value=42;
+console.log("Type of Value=42 : "+typeof value);
+
+value="Sujata";
+console.log("Type of value=Sujata : "+typeof value);
+
+value=10.2;
+console.log("Type of value=10.2 : "+typeof value);
+
+value='A';
+console.log("Type of value=A : "+typeof value);
+
+value=false
+console.log("Type of value=false : "+typeof value);
+
+value=null;
+console.log("Type of value=null : "+typeof value);
+
+var value1;
+console.log("Type of value1 with no assignment : "+typeof value1);
+
---
+++ Day 14(Java Script)/Type Cohersion.js
@@ -0,0 +1,32 @@
+var value1=10;
+var value2="10";
+console.log("Type of value1 : "+ typeof value1);
+console.log("Type of value2 : "+typeof value2);
+
+if(value1==value2){
+  console.log("value1 and value2 are equal");
+}
+else{
+  console.log("value1 and value 2 are not equal");
+}
+
+if(value1===value2){
+  console.log("value1 and value2 are equal");
+}
+else{
+  console.log("value1 and value 2 are not equal");
+}
+
+var product=value1*value2;
+console.log("product : "+product);
+
+
+var sum=value1+value2;
+console.log("sum : "+sum);
+
+var value3="Sujata";
+var prd=value1*value3;
+console.log("prd : "+prd);
+
+
+
